86f11fa3c2ebbe9000d6cd2d15963232ad8a6971
emcahell <emmanuelltowers@gmail.com>
2025-04-13 21:46:07 -0400

    image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,19 +9,19 @@ function Card({ data }) {
   let borderColor = ''
   switch (network) {
     case 'facebook':
-      iconPath = './assets/icon-facebook.svg' 
+      iconPath = '/src/assets/icon-facebook.svg' 
       borderColor = 'border-Facebook'
       break
     case 'twitter':
-      iconPath = './assets/icon-twitter.svg'
+      iconPath = '/src/assets/icon-twitter.svg'
       borderColor = 'border-Twitter'
       break
     case 'instagram':
-      iconPath = './assets/icon-instagram.svg'
+      iconPath = '/src/assets/icon-instagram.svg'
       borderColor = 'border-Instagram-Gradient-2'
       break
     case 'youtube':
-      iconPath = './assets/icon-youtube.svg'
+      iconPath = '/src/assets/icon-youtube.svg'
       borderColor = 'border-YouTube'
       break
     default:
